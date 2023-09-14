a6d5685e46b8e8d82e258b032ee89053697b69fb
Grant Sander <gksander93@gmail.com>
2023-09-14 09:49:59 -0500

    Update Definition.tsx

--- src/components/blog/Definition.tsx
+++ src/components/blog/Definition.tsx
@@ -12,15 +12,15 @@ type DefinitionProps = {
 
 export function Definition({ term, children, source }: DefinitionProps) {
   return (
-    <div class="p-3 -ml-3 bg-gradient-to-tl from-gray-100 via-transparent to-transparent rounded overflow-hidden not-prose flex flex-col gap-y-2">
+    <div class="p-3 -ml-3 bg-gradient-to-tl from-gray-100 dark:from-gray-700 dark:to-30% via-transparent to-transparent rounded overflow-hidden not-prose flex flex-col gap-y-2">
       <div class="flex items-center justify-between text-sm">
-        <span class="font-bold text-gray-600">{term}</span>
+        <span class="font-bold text-gray-300">{term}</span>
         {source && (
           <a
             href={source.url}
             target="_blank"
             rel="noreferrer"
-            class="text-secondary-800 flex items-center gap-x-1"
+            class="text-secondary-800 dark:text-secondary-300 flex items-center gap-x-1"
           >
             <span>source</span>
             <FaExternalLink className="w-2" />
